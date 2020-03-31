46a4eb3b7fe6675f7fcdd39163a661ccf7303c9e
Danila <61559724+Saxez@users.noreply.github.com>
2020-03-31 12:09:07 +0300

    с else

--- 17lw/171.pas
+++ 17lw/171.pas
@@ -10,37 +10,17 @@ BEGIN{ReadDigit}
   THEN
     BEGIN
       READ(F, Ch); 
-      IF (Ch = '0')
-      THEN
-        D := 0;
-      IF (Ch = '1')
-      THEN
-        D := 1; 
-      IF (Ch = '2')
-      THEN
-        D := 2;
-      IF (Ch = '3')
-      THEN
-        D := 3;
-      IF (Ch = '4')
-      THEN
-        D := 4;
-      IF (Ch = '5')
-      THEN
-        D := 5;
-      IF (Ch = '6')
-      THEN
-        D := 6;
-      IF (Ch = '7')
-      THEN
-        D := 7;
-      IF (Ch = '8')
-      THEN
-        D := 8;
-      IF (Ch = '9')
-      THEN
-        D := 9              
-      END
+      IF Ch = '0' THEN D := 0 ELSE
+      IF Ch = '1' THEN D := 1 ELSE
+      IF Ch = '2' THEN D := 2 ELSE
+      IF Ch = '3' THEN D := 3 ELSE
+      IF Ch = '4' THEN D := 4 ELSE
+      IF Ch = '5' THEN D := 5 ELSE
+      IF Ch = '6' THEN D := 6 ELSE
+      IF Ch = '7' THEN D := 7 ELSE
+      IF Ch = '8' THEN D := 8 ELSE
+      D := 9              
+    END
 END;{ReadDigit}
 BEGIN{task1}
   Sum := 0;
