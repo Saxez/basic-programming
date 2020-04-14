27e653ea26661e18dd0a90d64c3df16a41d80dbb
Danila <61559724+Saxez@users.noreply.github.com>
2020-04-14 19:00:23 +0300

    LW19

--- 17lw/171.pas
+++ 17lw/171.pas
@@ -1,35 +1,21 @@
 PROGRAM Task1(INPUT, OUTPUT);
+CONST
+  Max = 100;
 VAR
-  Sum, Digit: INTEGER;
-PROCEDURE ReadDigit(VAR F: TEXT; VAR D: INTEGER);
-VAR
-  Ch: CHAR;
-BEGIN{ReadDigit}
-  D := -1;   
-  IF NOT EOLN(F) 
-  THEN
+  IntSet: SET OF 2..Max;
+  Counter, Position: INTEGER;
+BEGIN{Task1}
+  Position := 2;
+  IntSet : [2..Max];  
+  WRITE('all prime numbers from 2 to ', Max,': ');
+  WHILE (IntSet <> [])
     BEGIN
-      READ(F, Ch); 
-      IF Ch = '0' THEN D := 0 ELSE
-      IF Ch = '1' THEN D := 1 ELSE
-      IF Ch = '2' THEN D := 2 ELSE
-      IF Ch = '3' THEN D := 3 ELSE
-      IF Ch = '4' THEN D := 4 ELSE
-      IF Ch = '5' THEN D := 5 ELSE
-      IF Ch = '6' THEN D := 6 ELSE
-      IF Ch = '7' THEN D := 7 ELSE
-      IF Ch = '8' THEN D := 8 ELSE
-      IF Ch = '9' THEN D := 9            
+      WHILE (Counter <= Max)
+        BEGIN
+          IF ((Position MOD Counter) = 0) 
+          THEN
+            IntSet := IntSet - [Counter];
+        END
+      WRITE(Position, ', ')
     END
-END;{ReadDigit}
-BEGIN{task1}
-  Sum := 0;
-  ReadDigit(INPUT, Digit);               
-  WHILE (Digit <> -1)
-  DO
-    BEGIN
-      Sum := Sum + Digit;
-      ReadDigit(INPUT, Digit);
-    END;
-  WRITELN(Sum)
-END.{task1}
+END.{Task1}
---
+++ 19lw/191.pas
@@ -0,0 +1,31 @@
+PROGRAM Task1(INPUT, OUTPUT);
+CONST
+  Max = 100;
+VAR
+  IntSet: SET OF 2..Max;
+  Counter, Position: INTEGER;
+BEGIN
+  IntSet := [2..Max];
+  Position := 2;
+  Counter := 2;
+  WHILE (Counter <= Max)
+  DO
+    BEGIN
+      Position := 2;
+      IF (Counter IN IntSet)
+      THEN
+        BEGIN
+          WRITE(Counter, ' ');
+          WHILE (Position <= Max)
+          DO
+            BEGIN
+              IF (Position MOD Counter = 0)
+              THEN
+                IntSet := IntSet - [Position];
+              Position := Position +1
+            END  
+        END;
+      Counter := Counter + 1
+    END;
+  READLN
+END.
